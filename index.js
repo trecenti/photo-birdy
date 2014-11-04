088d9df924aa3ca83d4203e6ecd68ff6fb0bf682
Bruno Trecenti <btrecent@thoughtworks.com>
2014-11-04 15:27:43 -0200

    using upload_with_image and fixed utf-8 issues

--- index.js
+++ index.js
@@ -1,46 +1,30 @@
 var watch = require('watch')
   , fs = require('fs')
-  , request = require('request')
-  , utf8 = require('utf8');
+  , request = require('request');
 
 // Twitter OAuth
 var oauth = {
-  consumer_key: 'consumer_key',
-  consumer_secret: 'consumer_secret',
-  token: 'token',
-  token_secret: 'token_secret'
+  consumer_key: process.env.CONSUMER_KEY || 'consumer_key',
+  consumer_secret: process.env.CONSUMER_SECRET || 'consumer_secret',
+  token: process.env.TOKEN || 'token',
+  token_secret: process.env.TOKEN_SECRET || 'token_secret'
 };
 
-function updateStatus(mediaId) {
+// Status Message
+var statusMessage = process.argv[2] || 'Criando Laços! #TWAL2014';
+
+function updateStatusWithMedia(file) {
   var statusOptions = {
-    url: 'https://api.twitter.com/1.1/statuses/update.json',
-    formData: { status: utf8.encode('New photo!'), media_ids: mediaId },
+    url: 'https://api.twitter.com/1.1/statuses/update_with_media.json',
+    formData: { status: statusMessage, 'media[]': fs.readFileSync(file) },
+    headers: { 'Accept-Charset': 'ISO-8859-1,utf-8' },
     oauth: oauth
   };
 
   request.post(statusOptions, function(err, res, body) {
     if (err) { throw err; }
 
-    console.log('status update: ', res.statusCode);
-  });
-}
-
-function uploadImage(file) {
-  var uploadOptions = {
-    url:'https://upload.twitter.com/1.1/media/upload.json',
-    oauth:oauth,
-    formData: { media: fs.createReadStream(file) }
-  };
-
-  request.post(uploadOptions, function(err, res, body) {
-    var mediaId;
-
-    if (err) { throw err; }
-
-    console.log('upload image: ', res.statusCode);
-    mediaId = JSON.parse(body).media_id.toString();
-
-    updateStatus(mediaId);
+    console.log('photo status update: ', res.statusCode);
   });
 }
 
@@ -48,6 +32,6 @@ watch.createMonitor('./photos', function (monitor) {
   monitor.on("created", function (file, stat) {
     console.log('new file:', file);
 
-    uploadImage(file);
+    updateStatusWithMedia(file);
   });
 })
